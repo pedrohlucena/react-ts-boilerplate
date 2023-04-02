181478ec2ec7fb69bb04ede5087d5d58d6e351e3
Pedro H. Lucena <pedrohlucena413@gmail.com>
2023-04-02 12:56:41 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import { Component } from "./components/Component/Component";
+
+export function App() {
+  return <Component />;
+}
---
+++ src/components/Component/Component.tsx
@@ -0,0 +1,5 @@
+import styles from "./Component.module.css";
+
+export function Component() {
+  return <h1 className={styles.component}>Hello World!</h1>;
+}
